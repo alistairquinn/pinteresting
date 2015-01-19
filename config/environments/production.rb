6651b3d6742a6b4bdfef167d447396e65e231500
Alistair Quinn <alistair.quinn@wishbubble.co.uk>
2015-01-19 17:05:47 +0000

    Added email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,18 @@ Rails.application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  authentication: "plain",
+  user_name: "alistaircquinn@gmail.com",
+  password: ENV['SMTP_PASSWORD'],
+  enable_starttls_auto: true
+  }
+
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
